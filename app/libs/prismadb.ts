ea69db651741d011a3ed25e283b599b555601846
Said Ombirik <saidombirik@gmail.com>
2023-07-08 00:14:35 +0100

    do

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | null;
 }
 
 const client = globalThis.prisma || new PrismaClient();
