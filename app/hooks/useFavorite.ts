d5b772a09de65973582b7ead6617314cb72ec6ce
Said Ombirik <saidombirik@gmail.com>
2023-07-08 09:04:11 +0100

    favorite

--- app/hooks/useFavorite.ts
+++ app/hooks/useFavorite.ts
@@ -14,19 +14,14 @@ interface IUseFavorite {
 
 const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
   const router = useRouter();
-
   const loginModal = useLoginModal();
-
   const hasFavorited = useMemo(() => {
     const list = currentUser?.favoriteIds || [];
-
     return list.includes(listingId);
   }, [currentUser, listingId]);
-
   const toggleFavorite = useCallback(
     async (e: React.MouseEvent<HTMLDivElement>) => {
       e.stopPropagation();
-
       if (!currentUser) {
         return loginModal.onOpen();
       }
@@ -35,9 +30,9 @@ const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
         let request;
 
         if (hasFavorited) {
-          request = () => axios.delete(`/api/favorites/${listingId}`);
+          request = () => axios.delete(`../api/favorites/${listingId}`);
         } else {
-          request = () => axios.post(`/api/favorites/${listingId}`);
+          request = () => axios.post(`../api/favorites/${listingId}`);
         }
 
         await request();
